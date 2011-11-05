220aedaf8aa358796179fa8b1bd9c40dfb412fde
Yasuharu NAKANO <ynak@jggug.org>
2011-11-05 14:22:12 +0900

    [from now] 2011/11/05 14:22:12
    diff --git a/src/test/groovy/PlayerSpec.groovy b/src/test/groovy/PlayerSpec.groovy
    index a6540ee..9a69764 100644
    --- a/src/test/groovy/PlayerSpec.groovy
    +++ b/src/test/groovy/PlayerSpec.groovy
    @@ -2,8 +2,13 @@ import spock.lang.*
    
     class PlayerSpec extends Specification {
    
    -  def "test"() {
    +  def "打席数と打数と案段数を受け取り、打率を計算する"() {
    +    setup:
    +    def player = new Player(box:3, atBat:2, hit:1)
    +
         expect:
    -    true
    +    player.getBattingAverage() == 0.5
       }
    +
    +  //def "打席数と打数と案段数を受け取り、小数第四位で四捨五入する"() {
     }

--- src/test/groovy/PlayerSpec.groovy
+++ src/test/groovy/PlayerSpec.groovy
@@ -2,8 +2,13 @@ import spock.lang.*
 
 class PlayerSpec extends Specification {
 
-  def "test"() {
+  def "打席数と打数と案段数を受け取り、打率を計算する"() {
+    setup:
+    def player = new Player(box:3, atBat:2, hit:1)
+
     expect:
-    true
+    player.getBattingAverage() == 0.5
   }
+
+  //def "打席数と打数と案段数を受け取り、小数第四位で四捨五入する"() {
 }
