ab29940f6c4ac2042d215c8089ae7475ef260837
Yasuharu NAKANO <ynak@jggug.org>
2011-11-05 16:38:25 +0900

    [from now] 2011/11/05 16:38:25
    diff --git a/src/main/groovy/ScorePrinter.groovy b/src/main/groovy/ScorePrinter.groovy
    index bbb579e..4ef00e1 100644
    --- a/src/main/groovy/ScorePrinter.groovy
    +++ b/src/main/groovy/ScorePrinter.groovy
    @@ -2,10 +2,10 @@ class ScorePrinter{
         def format(battingAverage) {
             if (battingAverage == null) {
                 return "----"
    -        }else if (battingAverage  < 1.00 ){
    -           return battingAverage.toString().replaceFirst(/^0/, '')
    -        }else{
    -          return "1.00"
             }
    +        if (battingAverage >= 1) {
    +            return "1.00"
    +        }
    +        return battingAverage.toString().replaceFirst(/^0/, '')
         }
     }

--- src/main/groovy/ScorePrinter.groovy
+++ src/main/groovy/ScorePrinter.groovy
@@ -2,10 +2,10 @@ class ScorePrinter{
     def format(battingAverage) {
         if (battingAverage == null) {
             return "----"
-        }else if (battingAverage  < 1.00 ){
-           return battingAverage.toString().replaceFirst(/^0/, '')
-        }else{
-          return "1.00"
         }
+        if (battingAverage >= 1) {
+            return "1.00"
+        }
+        return battingAverage.toString().replaceFirst(/^0/, '')
     }
 }
