0bb4964e03d44b1e235cc0b6f631c158c8dfd229
Yasuharu NAKANO <ynak@jggug.org>
2011-11-05 14:45:20 +0900

    [from now] 2011/11/05 14:45:20
    diff --git a/src/test/groovy/PlayerSpec.groovy b/src/test/groovy/PlayerSpec.groovy
    index b604a2a..24ae36c 100644
    --- a/src/test/groovy/PlayerSpec.groovy
    +++ b/src/test/groovy/PlayerSpec.groovy
    @@ -4,18 +4,15 @@ class PlayerSpec extends Specification {
    
       def "打席数と打数と案段数を受け取り、打率を計算する"() {
         setup:
    -    def player = new Player(box:3, atBat:2, hit:1)
    +    def player = new Player(box:box, atBat:atBat, hit:hit)
    
         expect:
    -    player.getBattingAverage() == 0.5
    -  }
    -
    -  def "打席数と打数と案段数を受け取り、打率を計算する 2"() {
    -    setup:
    -    def player = new Player(box:4, atBat:4, hit:1)
    +    player.getBattingAverage() == battingAverage
    
    -    expect:
    -    player.getBattingAverage() == 0.25
    +    where:
    +    box | atBat | hit | battingAverage
    +    3   | 2     | 1   | 0.5
    +    4   | 4     | 1   | 0.25
       }
    
       def "打席数と打数と案段数を受け取り、小数第四位で四捨五入する (無限小数、切り捨て)"() {

--- src/test/groovy/PlayerSpec.groovy
+++ src/test/groovy/PlayerSpec.groovy
@@ -4,18 +4,15 @@ class PlayerSpec extends Specification {
 
   def "打席数と打数と案段数を受け取り、打率を計算する"() {
     setup:
-    def player = new Player(box:3, atBat:2, hit:1)
+    def player = new Player(box:box, atBat:atBat, hit:hit)
 
     expect:
-    player.getBattingAverage() == 0.5
-  }
+    player.getBattingAverage() == battingAverage
 
-  def "打席数と打数と案段数を受け取り、打率を計算する 2"() {
-    setup:
-    def player = new Player(box:4, atBat:4, hit:1)
-
-    expect:
-    player.getBattingAverage() == 0.25
+    where:
+    box | atBat | hit | battingAverage
+    3   | 2     | 1   | 0.5
+    4   | 4     | 1   | 0.25
   }
 
   def "打席数と打数と案段数を受け取り、小数第四位で四捨五入する (無限小数、切り捨て)"() {
