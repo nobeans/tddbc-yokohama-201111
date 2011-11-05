3ebb064f9588b9da52a65afea41d76d4bf6162f2
Yasuharu NAKANO <ynak@jggug.org>
2011-11-05 17:28:27 +0900

    [from now] 2011/11/05 17:28:27
    diff --git a/src/test/groovy/ScorePrinterSpec.groovy b/src/test/groovy/ScorePrinterSpec.groovy
    index b16ba91..401df91 100644
    --- a/src/test/groovy/ScorePrinterSpec.groovy
    +++ b/src/test/groovy/ScorePrinterSpec.groovy
    @@ -2,18 +2,19 @@
     import spock.lang.*
    
     class ScorePrinterSpec extends Specification {
    -    def "打率数を計算しない場合、「---」と出力すること"() {
    -        setup:
    -        def scorePrinter = new ScorePrinter()
    
    +    def scorePrinter
    +
    +    def setup() {
    +        scorePrinter = new ScorePrinter()
    +    }
    +
    +    def "打率数を計算しない場合、「---」と出力すること"() {
             expect:
             scorePrinter.format(null) == "----"
         }
    
         def "打率が10割の場合は、「1.00」と出力すること"() {
    -        setup:
    -        def scorePrinter = new ScorePrinter()
    -
             expect:
             scorePrinter.format(battingAverage) == "1.00"
    
    @@ -22,9 +23,6 @@ class ScorePrinterSpec extends Specification {
         }
    
         def "打率が10割未満の場合は、先頭の0を省略して「.333」のように出力すること"() {
    -        setup:
    -        def scorePrinter = new ScorePrinter()
    -
             expect:
             scorePrinter.format(battingAverage) == result

--- src/test/groovy/ScorePrinterSpec.groovy
+++ src/test/groovy/ScorePrinterSpec.groovy
@@ -2,18 +2,19 @@
 import spock.lang.*
 
 class ScorePrinterSpec extends Specification {
-    def "打率数を計算しない場合、「---」と出力すること"() {
-        setup:
-        def scorePrinter = new ScorePrinter()
 
+    def scorePrinter
+
+    def setup() {
+        scorePrinter = new ScorePrinter()
+    }
+
+    def "打率数を計算しない場合、「---」と出力すること"() {
         expect:
         scorePrinter.format(null) == "----"
     }
 
     def "打率が10割の場合は、「1.00」と出力すること"() {
-        setup:
-        def scorePrinter = new ScorePrinter()
-
         expect:
         scorePrinter.format(battingAverage) == "1.00"
 
@@ -22,9 +23,6 @@ class ScorePrinterSpec extends Specification {
     }
 
     def "打率が10割未満の場合は、先頭の0を省略して「.333」のように出力すること"() {
-        setup:
-        def scorePrinter = new ScorePrinter()
-
         expect:
         scorePrinter.format(battingAverage) == result
 
