d45841fe7cf5786e76b89dc3a4f7ad128d206234
Yasuharu NAKANO <ynak@jggug.org>
2011-11-05 16:20:50 +0900

    [from now] 2011/11/05 16:20:50
    diff --git a/src/main/groovy/ScorePrinter.groovy b/src/main/groovy/ScorePrinter.groovy
    new file mode 100644
    index 0000000..87ad056
    --- /dev/null
    +++ b/src/main/groovy/ScorePrinter.groovy
    @@ -0,0 +1,8 @@
    +class ScorePrinter{
    +  def format(battingAverage){
    +   if( battingAverage == null ) {
    +	return "----"
    +   }
    +   return "1.00"
    +}
    +}
    \ No newline at end of file
    diff --git a/src/test/groovy/ScorePrinterSpec.groovy b/src/test/groovy/ScorePrinterSpec.groovy
    new file mode 100644
    index 0000000..1e31802
    --- /dev/null
    +++ b/src/test/groovy/ScorePrinterSpec.groovy
    @@ -0,0 +1,20 @@
    +
    +import spock.lang.*
    +
    +class ScorePrinterSpec extends Specification {
    +   def "打率数を計算しない場合、「---」と出力すること"(){
    +    setup:
    +	def scorePrinter = new ScorePrinter()
    +    expect:
    +	scorePrinter.format(null) == "----"
    +   }
    +
    +   def "打率が10割の場合は、「1.00」と出力すること"(){
    +    setup:
    +	def scorePrinter = new ScorePrinter()
    +    expect:
    +	scorePrinter.format(battingAverage) == "1.00"
    +    where:
    +    battingAverage << [1, 1.00, 1.0000]
    +   }
    +}
    \ No newline at end of file

---
+++ src/main/groovy/ScorePrinter.groovy
@@ -0,0 +1,8 @@
+class ScorePrinter{
+  def format(battingAverage){
+   if( battingAverage == null ) {
+	return "----"	
+   }
+   return "1.00"
+}
+}
---
+++ src/test/groovy/ScorePrinterSpec.groovy
@@ -0,0 +1,20 @@
+
+import spock.lang.*
+
+class ScorePrinterSpec extends Specification {
+   def "打率数を計算しない場合、「---」と出力すること"(){
+    setup:
+	def scorePrinter = new ScorePrinter()
+    expect:
+	scorePrinter.format(null) == "----"	
+   }
+
+   def "打率が10割の場合は、「1.00」と出力すること"(){
+    setup:
+	def scorePrinter = new ScorePrinter()
+    expect:
+	scorePrinter.format(battingAverage) == "1.00"
+    where:
+    battingAverage << [1, 1.00, 1.0000]
+   }
+}
